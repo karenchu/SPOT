7cf499a1f18976bb8f6ea77c760b35ca4b61f78f
karenchu <karen.alison.chu@gmail.com>
2014-12-15 12:44:56 -0800

    add public and public/stylesheets

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	app = express();
 
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 // our root route
 app.get("/", function (req, res) {
-	res.send("Herro");
+	res.render("sites/home");
 });
 
 app.listen(3000, function() {
