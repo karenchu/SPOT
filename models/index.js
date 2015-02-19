a185623b4225f5dce2dbc7071e0cbe88ecba419f
karenchu <karen.alison.chu@gmail.com>
2015-02-19 02:16:45 -0800

    adding production db

--- models/index.js
+++ models/index.js
@@ -8,12 +8,12 @@ var config    = require(__dirname + '/../config/config.json')[env];
 // NEEDED FOR HEROKU ///////////
 if(config.use_env_variable){
   var db_info = process.env[config.use_env_variable].match(/([^:]+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
-  config.dialect=db_info[1];
-  config.username=db_info[2];
-  config.password=db_info[3];
-  config.host=db_info[4];  
-  config.port=db_info[5];  
-  config.database=db_info[6];  
+      config.dialect=db_info[1];
+      config.username=db_info[2];
+      config.password=db_info[3];
+      config.host=db_info[4];  
+      config.port=db_info[5];  
+      config.database=db_info[6];  
 }
 //////////////////////////////
 console.error(config);
